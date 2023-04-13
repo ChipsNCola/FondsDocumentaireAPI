af86ddf2a6c2a05167ebcb97398dafb31340fefb
Chips009 <Chips009@MacBook-Air-de-Chips.local>
2023-04-13 07:53:27 +0000

    test

--- aicontent.py
+++ aicontent.py
@@ -3,7 +3,7 @@ import config
 import os
 from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper,ServiceContext
 from langchain import OpenAI
-os.environ['OPENAI_API_KEY'] = "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"
+
 index=GPTSimpleVectorIndex.load_from_disk("./model/index.json")
 def ask(question):
     reponse=index.query(question)
